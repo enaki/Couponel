87568306c4a2f0e6132425e29e713e05db51bfaf
Iurea Bogdan-Iulian <iureabogdan@gmail.com>
2020-07-20 23:23:04 +0300

    Added EntityFrameworkCore Dependencies and Added Entity+Student classes

---
+++ CouponelApp/CouponelServices/CouponelServices.Database/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CouponelServices.Entities
+{
+    public sealed class Student: Entity
+    {
+        public Student(string firstName, string lastName, string email, string phoneNumber)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+}
