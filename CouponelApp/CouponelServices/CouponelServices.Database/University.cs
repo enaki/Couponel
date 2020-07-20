c77f7b22236a0d46ee4b1f59dbf6a6e05b4c90a6
Iurea Bogdan-Iulian <iureabogdan@gmail.com>
2020-07-21 01:08:36 +0300

    Added CouponelContext class Added relations Fluent

--- CouponelApp/CouponelServices/CouponelServices.Database/University.cs
+++ CouponelApp/CouponelServices/CouponelServices.Database/University.cs
@@ -7,22 +7,30 @@ namespace CouponelServices.Entities
 {
     public sealed class University: Entity
     {
-        public University(string name, string address, string email, string phoneNumber)
+        public University(string name, string email, string phoneNumber)
         {
             Name = name;
-            Address = address;
+            Addresses = new List<Address>();
             Email = email;
             PhoneNumber = phoneNumber;
-
+            Faculties = new List<Faculty>();
         }
         [Required]
         public string Name { get; private set; }
-        [Required]
-        public string Address { get; private set; }
         [Required]
         public string Email { get; private set; }
         [Required]
         public string PhoneNumber { get; private set; }
 
+
+        #region Database Relations
+
+        [Required]
+        public ICollection<Faculty> Faculties { get; private set; }
+        
+        [Required]
+        public ICollection<Address> Addresses { get; private set; }
+
+        #endregion
     }
 }
