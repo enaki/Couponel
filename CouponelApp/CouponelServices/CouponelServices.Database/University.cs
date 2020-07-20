00879caa396a774972cfa1257dc2dfee471213fe
enaky <enachi98@gmail.com>
2020-07-20 23:58:02 +0300

    Added Address, University, User classes.

---
+++ CouponelApp/CouponelServices/CouponelServices.Database/University.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CouponelServices.Entities
+{
+    public sealed class University: Entity
+    {
+        public University(string name, string address, string email, string phoneNumber)
+        {
+            Name = name;
+            Address = address;
+            Email = email;
+            PhoneNumber = phoneNumber;
+
+        }
+        [Required]
+        public string Name { get; private set; }
+        [Required]
+        public string Address { get; private set; }
+        [Required]
+        public string Email { get; private set; }
+        [Required]
+        public string PhoneNumber { get; private set; }
+
+    }
+}
