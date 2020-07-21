d54df0fcd9ea6c7cbe51cdeaa7d9f7fc9d0c3dc3
Ursan <ursan_robert@yahoo.com>
2020-07-21 22:33:20 +0300

    Added data insertion sql scripts for University and Faculty tables.

---
+++ Scripts/DatabaseInsertion/University.sql
@@ -0,0 +1,109 @@
+INSERT INTO [dbo].[University]
+           ([Id]
+           ,[Name]
+           ,[Email]
+           ,[PhoneNumber])
+     VALUES
+           ( '9b43ec3c-6518-4f3f-82ad-0ec37644bbf5'
+           ,'Universitatea Tehnica Gheorghe Asachi'
+           ,'contact@tuiasi.ro' 
+           ,' +40-232-212322')
+
+INSERT INTO [dbo].[University]
+           ([Id]
+           ,[Name]
+           ,[Email]
+           ,[PhoneNumber])
+     VALUES
+           ( '21c36828-ef02-44f2-b5a7-e3a16250a39e'
+           ,'Universitatea de Medicina Grigore T. Popa'
+           ,'rectorat@umfiasi.ro' 
+           ,' +40.232.301.603')
+
+INSERT INTO [dbo].[University]
+           ([Id]
+           ,[Name]
+           ,[Email]
+           ,[PhoneNumber])
+     VALUES
+           ( '8d5f24cc-56b5-468c-b797-f84df5a666ab'
+           ,'Universitatea Alexandru Ioan Cuza'
+           ,'contact@uaic.ro' 
+           ,' 0232 20 1000')
+
+INSERT INTO [dbo].[University]
+           ([Id]
+           ,[Name]
+           ,[Email]
+           ,[PhoneNumber])
+     VALUES
+           ( 'a99ec720-cef6-41a3-a5f8-63432d3f76d5'
+           ,'Universitatea de Ştiinţe Agricole şi Medicină Veterinară "Ion Ionescu de la Brad"'
+           ,'rectorat@uaiasi.ro' 
+           ,' 0232 407.407')
+
+INSERT INTO [dbo].[University]
+           ([Id]
+           ,[Name]
+           ,[Email]
+           ,[PhoneNumber])
+     VALUES
+           ( 'c46f8bc2-05c9-400f-8118-536a8a93699d'
+           ,'Universitatea din Bucuresti'
+           ,'office@g.unibuc.ro' 
+           ,' +4021-313.17.60')
+
+INSERT INTO [dbo].[University]
+           ([Id]
+           ,[Name]
+           ,[Email]
+           ,[PhoneNumber])
+     VALUES
+           ( '35d19bc6-fee9-41cf-a64f-abb9c8cd891c'
+           ,'Academia de Studii Economice din Bucuresti'
+           ,'rectorat@ase.ro' 
+           ,' +40213191900')
+
+INSERT INTO [dbo].[University]
+           ([Id]
+           ,[Name]
+           ,[Email]
+           ,[PhoneNumber])
+     VALUES
+           ( 'c2c82fda-6221-4a36-9586-5cdf741eb0b4'
+           ,'Universitatea de Medicina si Farmacie Carol Davila'
+           ,'rectorat@umfcd.ro' 
+           ,' +4021 3180719')
+
+INSERT INTO [dbo].[University]
+           ([Id]
+           ,[Name]
+           ,[Email]
+           ,[PhoneNumber])
+     VALUES
+           ( '535a6e22-d462-497e-b793-3d18a06548e6'
+           ,'Universitatea de Vest'
+           ,'secretariat@e-uvt.ro' 
+           ,' +40-(0)256-592111')
+
+INSERT INTO [dbo].[University]
+           ([Id]
+           ,[Name]
+           ,[Email]
+           ,[PhoneNumber])
+     VALUES
+           ( 'b00cf8cf-c6e9-4438-be39-e97515e6b649'
+           ,'Universitatea Politehnica Timisoara'
+           ,'rector@upt.ro' 
+           ,'0256 - 403021')
+
+INSERT INTO [dbo].[University]
+           ([Id]
+           ,[Name]
+           ,[Email]
+           ,[PhoneNumber])
+     VALUES
+           ( 'f63c98a8-d6a7-44f1-9f8c-c1b7fd93b1c5'
+           ,'Universitatea de Medicina si Farmacie Victor Babes'
+           ,'rector@umft.ro' 
+           ,'+40.0256.204.400')
