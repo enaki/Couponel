50150841115e42243fcbc47940b0950d275d9475
Ursan <ursan_robert@yahoo.com>
2020-07-29 11:42:09 +0300

    Added Address Tests.

---
+++ CouponelApp/Couponel.IntegrationTests/AddressControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Couponel.API.Controllers;
+using Couponel.Business.Institutions.Addresses.Models;
+using Couponel.Entities;
+using Couponel.Persistence;
+using FluentAssertions;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Couponel.IntegrationTests
+{
+    public class AddressControllerTests : IntegrationTests
+    {
+        [Fact]
+        public async Task GetAddress()
+        {
+            var address = new Address("Romania", "Iasi", "Bucuriei", "42");
+
+            await ExecuteDatabaseAction(async (couponelContext) =>
+            {
+                await couponelContext.Addresses.AddAsync(address);
+                await couponelContext.SaveChangesAsync();
+            });
+
+            var response = await HttpClient.GetAsync($"api/address/{address.Id}");
+
+            response.IsSuccessStatusCode.Should().BeTrue();
+           
+            var addresses = await response.Content.ReadAsAsync<IList<AddressModel>>();
+
+            addresses.Should().HaveCount(1);
+
+        }
+    }
+
+}
---
+++ CouponelApp/Couponel.IntegrationTests/IntegrationTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Couponel.API;
+using Couponel.Persistence;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Couponel.IntegrationTests
+{
+    public class IntegrationTests : IAsyncLifetime
+    {
+        private readonly WebApplicationFactory<Startup> _webApplicationFactory;
+
+        protected HttpClient HttpClient { get; private set; }
+
+        public IntegrationTests()
+        {
+            _webApplicationFactory=new WebApplicationFactory<Startup>().WithWebHostBuilder(builder => { });
+            HttpClient = _webApplicationFactory.CreateClient();
+        }
+
+        protected async Task ExecuteDatabaseAction(Func<CouponelContext, Task> databaseAction)
+        {
+            using (var scope = _webApplicationFactory.Services.CreateScope())
+            {
+                var couponelContext = scope.ServiceProvider.GetRequiredService<CouponelContext>();
+
+                await databaseAction(couponelContext); 
+            }
+        }
+
+        public async Task CleanupDatabase(CouponelContext couponelContext)
+        {
+            couponelContext.Addresses.RemoveRange(couponelContext.Addresses);
+            couponelContext.Coupons.RemoveRange(couponelContext.Coupons);
+            couponelContext.Faculties.RemoveRange(couponelContext.Faculties);
+            couponelContext.Students.RemoveRange(couponelContext.Students);
+            couponelContext.Universities.RemoveRange(couponelContext.Universities);
+            couponelContext.Users.RemoveRange(couponelContext.Users);
+            await couponelContext.SaveChangesAsync();
+        }
+        public async Task InitializeAsync()
+        {
+            await ExecuteDatabaseAction(async (couponelContext) => await CleanupDatabase(couponelContext));
+        }
+
+        public Task DisposeAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
