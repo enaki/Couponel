8dc8b4891baae76852d94ae1c067a44414ba5798
Iurea Bogdan-Iulian <iureabogdan@gmail.com>
2020-07-31 15:02:05 +0300

    Updated StudentRepository and IStudentRepository

--- CouponelApp/Couponel.Persistance/Repositories/IdentitiesRepositories/StudentsRepository/IStudentsRepository.cs
+++ CouponelApp/Couponel.Persistance/Repositories/IdentitiesRepositories/StudentsRepository/IStudentsRepository.cs
@@ -1,10 +1,15 @@
 ﻿using Couponel.Entities.Identities.UserTypes;
 using Couponel.Persistence.Repositories.Repository;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Couponel.Persistence.Repositories.IdentitiesRepositories.StudentsRepository
 {
     public interface IStudentsRepository : IRepository<Student>
     {
-
+        public Task<IList<Student>> GetAllByFirstName(string firstName);
+        public Task<IList<Student>> GetAllByLastName(string lastName);
+        public Task<Student> GetByEmail(string email);
+        public Task<Student> GetByPhoneNumber(string phoneNumber);
     }
 }
