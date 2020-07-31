c0ed3cdb97b41597cd4eaa2b19bd0dbab811f341
Iurea Bogdan-Iulian <iureabogdan@gmail.com>
2020-07-31 14:44:04 +0300

    Updated FacultiesRepository and IFacultiesRepository

--- CouponelApp/Couponel.Persistance/Repositories/InstitutionsRepositories/FacultiesRepository/FacultiesRepository.cs
+++ CouponelApp/Couponel.Persistance/Repositories/InstitutionsRepositories/FacultiesRepository/FacultiesRepository.cs
@@ -1,12 +1,34 @@
 ﻿using Couponel.Entities.Institutions;
 using Couponel.Persistence.Repositories.Repository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
 
 namespace Couponel.Persistence.Repositories.InstitutionsRepositories.FacultiesRepository
 {
-    public sealed class FacultiesRepository: Repository<Faculty>, IFacultiesRepository
+    public sealed class FacultiesRepository : Repository<Faculty>, IFacultiesRepository
     {
         public FacultiesRepository(CouponelContext context) : base(context)
         {
         }
+        public async Task<Faculty> GetByIdWithAddressStudentsAndStudentsAdreesses(Guid id)
+            => await this.context.Faculties
+                .Include(faculty => faculty.Students)
+                    .ThenInclude(student => student.Address)
+                .Include(faculty => faculty.Address)
+                .FirstAsync(faculty => faculty.Id == id);
+        public async Task<Faculty> GetByIdWithAddressAndStudents(Guid id)
+            => await this.context.Faculties
+                .Include(faculty => faculty.Students)
+                .Include(faculty => faculty.Address)
+                .FirstAsync(faculty => faculty.Id == id);
+        public async Task<Faculty> GetByIdWithStudents(Guid id)
+            => await this.context.Faculties
+                .Include(faculty => faculty.Students)
+                .FirstAsync(faculty => faculty.Id == id);
+        public async Task<Faculty> GetByIdWithAddress(Guid id)
+            => await this.context.Faculties
+                .Include(faculty => faculty.Address)
+                .FirstAsync(faculty => faculty.Id == id);
     }
 }
--- CouponelApp/Couponel.Persistance/Repositories/InstitutionsRepositories/FacultiesRepository/IFacultiesRepository.cs
+++ CouponelApp/Couponel.Persistance/Repositories/InstitutionsRepositories/FacultiesRepository/IFacultiesRepository.cs
@@ -1,9 +1,16 @@
 ﻿using Couponel.Entities.Institutions;
 using Couponel.Persistence.Repositories.Repository;
+using System;
+using System.Threading.Tasks;
 
 namespace Couponel.Persistence.Repositories.InstitutionsRepositories.FacultiesRepository
 {
     public interface IFacultiesRepository: IRepository<Faculty>
     {
+        public Task<Faculty> GetByIdWithAddressStudentsAndStudentsAdreesses(Guid id);
+        public Task<Faculty> GetByIdWithAddressAndStudents(Guid id);
+        public Task<Faculty> GetByIdWithStudents(Guid id);
+        public Task<Faculty> GetByIdWithAddress(Guid id);
+        
     }
 }
