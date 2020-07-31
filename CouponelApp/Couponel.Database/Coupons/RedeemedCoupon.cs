d77c8e5bd3e9e4ae0c0b7ef68b3e21fc354f4dc3
Iurea Bogdan-Iulian <iureabogdan@gmail.com>
2020-08-01 01:43:35 +0300

    Modified Relations Between Coupon and RedeemedCoupon to One to One

--- CouponelApp/Couponel.Database/Coupons/RedeemedCoupon.cs
+++ CouponelApp/Couponel.Database/Coupons/RedeemedCoupon.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace Couponel.Entities.Coupons
 {
     public class RedeemedCoupon : Entity
     {
+        public RedeemedCoupon(string status)
+        {
+            Status = status;
+        }
         [Required]
         public DateTime ReedemedDate { get; private set; }
         [Required]
         public string Status { get; private set; }
+        public void Update(string status)
+        {
+            Status = status;
+        }
+        #region Database Relations
+        [Required]
+        [ForeignKey("Coupon")]
+        public Guid CouponId { get; private set; }
+        [Required]
+        public Coupon Coupon { get; private set; }
+        #endregion
     }
 }
