f5c427a4738c000ae84588e2b7fab49065cb947f
Iurea Bogdan-Iulian <iureabogdan@gmail.com>
2020-08-03 16:30:06 +0300

    Updated AddressController route

--- CouponelApp/Couponel.API/Controllers/InstitutionsController/AddressController.cs
+++ CouponelApp/Couponel.API/Controllers/InstitutionsController/AddressController.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Logging;
 
 namespace Couponel.API.Controllers
 {
-    [Route("[controller]")]
+    [Route("api/address")]
     [ApiController]
     public class AddressController : ControllerBase
     {
