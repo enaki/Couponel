bb6763ac5cba902d17ea146cc7df6c4279f5a015
Ursan <ursan_robert@yahoo.com>
2020-08-05 23:30:16 +0300

    Added Automation Test when credentials are valid for Login Page.

---
+++ CouponelApp/Couponel.AutomationTests/PageObjects/Dashboard/DashboardPage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.PageObjects;
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+namespace Couponel.AutomationTests.PageObjects.Dashboard
+{
+    public class DashboardPage
+    {
+        public IWebDriver driver;
+
+        public DashboardPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(this, new RetryingElementLocator(this.driver, TimeSpan.FromSeconds(10)));
+        }
+
+        [FindsBy(How = How.ClassName, Using = "flex-row-end")]
+        public IWebElement UserMenu { get; set; }
+
+        public void WaitForPageToLoad(string selector)
+        {
+            var wait=new WebDriverWait(driver,TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(selector)));
+        }
+    }
+}
---
+++ CouponelApp/Couponel.AutomationTests/Tests/LoginTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Couponel.AutomationTests.Helpers;
+using Couponel.AutomationTests.PageObjects.Dashboard;
+using Couponel.AutomationTests.PageObjects.Login;
+using Xunit;
+
+namespace Couponel.AutomationTests.Tests
+{
+    public class LoginTests: Browser, IDisposable
+    {
+        public LoginPage loginPage;
+        public DashboardPage dashboardPage;
+        public LoginTests()
+        {
+            Driver.Navigate().GoToUrl("http://localhost:4200/authentication");
+            loginPage=new LoginPage(Driver);
+        }
+
+        [Fact]
+        public void Login_With_Valid_Credentials()
+        {
+            loginPage.Login("mishu","mishu");
+            dashboardPage=new DashboardPage(Driver);
+            dashboardPage.WaitForPageToLoad("[class='flex-row-end']");
+            Assert.True(dashboardPage.UserMenu.Displayed);
+        }
+        public void Dispose()
+        {
+            CloseBrowser();
+        }
+    }
+}
