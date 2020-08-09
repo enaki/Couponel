76973b6c652d29f62af6c368a5c2aff32d117638
Iurea Bogdan-Iulian <iureabogdan@gmail.com>
2020-08-10 00:04:16 +0300

    Added role-based dashboard

--- CouponelApp/Couponel.Web/src/app/dashboard/dashboard/dashboard.component.ts
+++ CouponelApp/Couponel.Web/src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
+import {VoucherModel} from "../../voucher/models";
 
 
 @Component({
@@ -8,10 +9,31 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard.component.scss']
 })
 
-export class DashboardComponent{
+export class DashboardComponent implements OnInit, OnDestroy{
+  isStudent: boolean;
+  isOfferer: boolean;
+  isAdmin: boolean;
+
   constructor(private router: Router) {}
 
   public goToPage(page: string): void {
     this.router.navigate([page]);
+  }
+
+  ngOnInit(): void {
+    if (localStorage.getItem('userRole') === 'Student')
+    {
+      this.isStudent = true;
+    }
+    else if (localStorage.getItem('userRole') === 'Offerer')
+    {
+      this.isOfferer = true;
+    }
+    else if (localStorage.getItem('userRole') === 'Admin')
+    {
+      this.isAdmin = true;
+    }
+  }
+  ngOnDestroy(): void {
   }
 }
