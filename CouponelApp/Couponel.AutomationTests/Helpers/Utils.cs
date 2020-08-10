7d2a65b35fe3e29c35017f3de4805cfa729fdc5c
Ursan <ursan_robert@yahoo.com>
2020-08-10 13:49:25 +0300

    Added Automation Tests for register form. Found one bug at email. Example: "email@gmai" is valid, but it shouldn't

---
+++ CouponelApp/Couponel.AutomationTests/Helpers/Utils.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Linq;
+
+namespace Couponel.AutomationTests.Helpers
+{
+    public static class Utils
+    {
+        private static readonly Random randomGenerator = new Random();
+
+        public static string GetRandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(
+                Enumerable.Repeat(chars, length).Select(s => s[randomGenerator.Next(s.Length)]).ToArray());
+        }
+        public static string GetRandomPhoneNumber(int length)
+        {
+            const string chars = "0123456789";
+            return new string(
+                Enumerable.Repeat(chars, length).Select(s => s[randomGenerator.Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ CouponelApp/Couponel.AutomationTests/Tests/RegisterTests.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Couponel.AutomationTests.Helpers;
+using Couponel.AutomationTests.PageObjects.Dashboard;
+using Couponel.AutomationTests.PageObjects.Login;
+using Couponel.AutomationTests.PageObjects.Register;
+using Xunit;
+
+namespace Couponel.AutomationTests.Tests
+{
+    [Collection("Sequential")]
+    public class RegisterTests:Browser, IDisposable
+    {
+        public RegisterPage registerPage;
+        public DashboardPage dashboardPage;
+        public RegisterTests()
+        {
+            Driver.Navigate().GoToUrl("http://localhost:4200/authentication");
+            registerPage = new RegisterPage(Driver);
+        }
+
+        [Fact]
+        public void Register_Student_With_Valid_Credentials()
+        {
+            var user = Utils.GetRandomString(6);
+            registerPage.CheckboxRegister.Click();
+            registerPage.TxtUsername.SendKeys(user);
+            registerPage.TxtFirstName.SendKeys(user);
+            registerPage.TxtLastName.SendKeys(user);
+            registerPage.TxtPassword.SendKeys(user);
+            registerPage.TxtEmail.SendKeys(user + "@gmail.com");
+            registerPage.DropDownRole.Click();
+            registerPage.TxtPhoneNumber.SendKeys(Utils.GetRandomPhoneNumber(10));
+            registerPage.ValueStudent.Click();
+            registerPage.BtnRegister.Click();
+            dashboardPage = new DashboardPage(Driver);
+            dashboardPage.WaitForPageToLoad("[class='flex-row-end']");
+            Assert.True(dashboardPage.UserMenu.Displayed);
+        }
+        [Fact]
+        public void Register_Student_With_Invalid_Email()
+        {
+            var user = Utils.GetRandomString(6);
+            registerPage.CheckboxRegister.Click();
+            registerPage.TxtUsername.SendKeys(user);
+            registerPage.TxtFirstName.SendKeys(user);
+            registerPage.TxtLastName.SendKeys(user);
+            registerPage.TxtPassword.SendKeys(user);
+            registerPage.TxtEmail.SendKeys(user);
+            registerPage.DropDownRole.Click();
+            registerPage.TxtPhoneNumber.SendKeys(Utils.GetRandomPhoneNumber(10));
+            registerPage.ValueStudent.Click();
+            registerPage.BtnRegister.Click();
+            Assert.True(registerPage.EmailInvalidCred.Displayed);
+        }
+
+        [Fact]
+        public void Register_Student_With_Invalid_Email_Bug()
+        {
+            var user = Utils.GetRandomString(6);
+            registerPage.CheckboxRegister.Click();
+            registerPage.TxtUsername.SendKeys(user);
+            registerPage.TxtFirstName.SendKeys(user);
+            registerPage.TxtLastName.SendKeys(user);
+            registerPage.TxtPassword.SendKeys(user);
+            registerPage.TxtEmail.SendKeys(user+"@gmai");
+            registerPage.DropDownRole.Click();
+            registerPage.TxtPhoneNumber.SendKeys(Utils.GetRandomPhoneNumber(10));
+            registerPage.ValueStudent.Click();
+            registerPage.BtnRegister.Click();
+            Assert.True(registerPage.EmailInvalidCred.Displayed);
+        }
+        [Fact]
+        public void Register_Student_With_Invalid_Credentials()
+        {
+            var user = Utils.GetRandomString(3);
+            registerPage.CheckboxRegister.Click();
+            registerPage.TxtUsername.SendKeys(user);
+            registerPage.TxtFirstName.SendKeys(user);
+            registerPage.TxtLastName.SendKeys(user);
+            registerPage.TxtPassword.SendKeys(user);
+            registerPage.TxtEmail.SendKeys(user);
+            registerPage.DropDownRole.Click();
+            registerPage.TxtPhoneNumber.SendKeys(Utils.GetRandomPhoneNumber(10));
+            registerPage.ValueStudent.Click();
+            registerPage.BtnRegister.Click();
+            Assert.True(registerPage.EmailInvalidCred.Displayed);
+        }
+        [Fact]
+        public void Register_Offerer_With_Valid_Credentials()
+        {
+            var user = Utils.GetRandomString(6);
+            registerPage.CheckboxRegister.Click();
+            registerPage.TxtUsername.SendKeys(user);
+            registerPage.TxtFirstName.SendKeys(user);
+            registerPage.TxtLastName.SendKeys(user);
+            registerPage.TxtPassword.SendKeys(user);
+            registerPage.TxtEmail.SendKeys(user + "@gmail.com");
+            registerPage.DropDownRole.Click();
+            registerPage.TxtPhoneNumber.SendKeys(Utils.GetRandomPhoneNumber(10));
+            registerPage.ValueStudent.Click();
+            registerPage.BtnRegister.Click();
+            dashboardPage = new DashboardPage(Driver);
+            dashboardPage.WaitForPageToLoad("[class='flex-row-end']");
+            Assert.True(dashboardPage.UserMenu.Displayed);
+        }
+        public void Dispose()
+        {
+            CloseBrowser();
+        }
+    }
+}
