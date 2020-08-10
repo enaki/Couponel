7f18f9417b3bd3080ba0a16fa528e5187f48f1e9
gbtAAA <gabriela.mih10@gmail.com>
2020-08-10 20:35:58 +0300

    Added profile info.v1

---
+++ CouponelApp/Couponel.Web/src/app/profile/models/address.model.ts
@@ -0,0 +1,6 @@
+export type AddressModel = {
+  country: string;
+  city: string;
+  street: string;
+  number: string;
+};
---
+++ CouponelApp/Couponel.Web/src/app/profile/services/profile-info.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProfileInfoModel } from '../models/profile-info.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProfileInfoService {
+  public endpoint =
+        'https://localhost:5001/api/users';
+        private httpOptions = {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${JSON.parse(localStorage.getItem('userToken'))}`
+          })
+        };
+  constructor(private readonly httpClient: HttpClient) {}
+
+  public getProfilInfo(userId: string): Observable<ProfileInfoModel> {
+    return this.httpClient.get<ProfileInfoModel>(`${this.endpoint}/${userId}`,this.httpOptions);
+
+  }
+  //sa creez o noua metoda de update care sa apeleze endpointu ptr a updata
+
+
+
+
+}
