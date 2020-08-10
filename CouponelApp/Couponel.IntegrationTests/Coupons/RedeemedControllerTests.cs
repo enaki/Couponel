aea7d575cc586c396636ee37a629b6b9a4b6a0fc
Ursan <ursan_robert@yahoo.com>
2020-08-10 17:26:21 +0300

    Finished Integration Tests.

--- CouponelApp/Couponel.IntegrationTests/Coupons/RedeemedControllerTests.cs
+++ CouponelApp/Couponel.IntegrationTests/Coupons/RedeemedControllerTests.cs
@@ -48,5 +48,73 @@ namespace Couponel.IntegrationTests.Coupons
             redeemedcoupons.Should().NotBeNull();
 
         }
+        [Fact]
+        public async Task UpdateRedeemedCoupon()
+        {
+            // Arrange
+            var offerer = UserRegisterModelFactory.getUserFactory("Offerer").getUser();
+            var coupon = CouponModelFactory.Default();
+            offerer.AddCoupon(coupon);
+            var student = new Student(this.AuthenticatedUserId);
+            var admin = UserRegisterModelFactory.getUserFactory("Admin").getUser();
+            var university = InstitutionsModelFactory.University();
+            var faculty = InstitutionsModelFactory.Faculty();
+            university.AddFaculty(faculty);
+            faculty.AddStudent(student);
+
+            var redeemedcoupon = new RedeemedCoupon("Used", coupon.Id);
+            student.AddRedeemedCoupon(redeemedcoupon);
+            redeemedcoupon.UpdateStatus("Expired");
+            await ExecuteDatabaseAction(async couponelContext =>
+            {
+                await couponelContext.Users.AddAsync(admin);
+                await couponelContext.Users.AddAsync(offerer);
+                await couponelContext.Universities.AddAsync(university);
+                await couponelContext.SaveChangesAsync();
+            });
+
+            //Act
+            var response = await HttpClient.GetAsync($"/api/redeemedCoupons/{redeemedcoupon.Id}");
+
+            // Assert
+            response.IsSuccessStatusCode.Should().BeTrue();
+            var redeemedcoupons = await response.Content.ReadAsAsync<RedeemedCoupon>();
+            redeemedcoupons.Status.Should().BeEquivalentTo("Expired");
+
+        }
+        [Fact]
+        public async Task DeleteRedeemedCoupon()
+        {
+            // Arrange
+            var offerer = UserRegisterModelFactory.getUserFactory("Offerer").getUser();
+            var coupon = CouponModelFactory.Default();
+            offerer.AddCoupon(coupon);
+            var student = new Student(this.AuthenticatedUserId);
+            var admin = UserRegisterModelFactory.getUserFactory("Admin").getUser();
+            var university = InstitutionsModelFactory.University();
+            var faculty = InstitutionsModelFactory.Faculty();
+            university.AddFaculty(faculty);
+            faculty.AddStudent(student);
+
+            var redeemedcoupon = new RedeemedCoupon("Used", coupon.Id);
+            student.AddRedeemedCoupon(redeemedcoupon);
+            redeemedcoupon.UpdateStatus("Expired");
+            await ExecuteDatabaseAction(async couponelContext =>
+            {
+                await couponelContext.Users.AddAsync(admin);
+                await couponelContext.Users.AddAsync(offerer);
+                await couponelContext.Universities.AddAsync(university);
+                await couponelContext.SaveChangesAsync();
+            });
+
+            //Act
+            var response = await HttpClient.DeleteAsync($"/api/redeemedCoupons/{redeemedcoupon.Id}");
+
+            // Assert
+            response.IsSuccessStatusCode.Should().BeTrue();
+            var redeemedcoupons = await response.Content.ReadAsAsync<RedeemedCoupon>();
+            redeemedcoupons.Should().BeNull();
+
+        }
     }
 }
